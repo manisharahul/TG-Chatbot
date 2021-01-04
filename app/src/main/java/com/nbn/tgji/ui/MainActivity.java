b4e29baa4b3cb7f178f46332d4eb8496e77060b7
Sharad Tawde <59132807+milkiesharad@users.noreply.github.com>
2021-01-04 20:20:58 +0530

    Initial commit

---
+++ app/src/main/java/com/nbn/tgji/ui/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nbn.tgji.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nbn.tgji.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
